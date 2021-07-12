f7dc6f4e70ff4464ca9fefd35f99dab1e6b05abe
BestPlayerVietnam <45295306+BestPlayerVietnam@users.noreply.github.com>
2021-07-12 16:01:35 +0700

    Add files via upload

---
+++ CellAuto/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace CellAuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GenerateRandomLife = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.nextgeneration = new System.Windows.Forms.Button();
+            this.kill = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pauseTime = new System.Windows.Forms.Button();
+            this.counter = new System.Windows.Forms.Label();
+            this.counterNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeight = 30;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 10;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(983, 353);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // GenerateRandomLife
+            // 
+            this.GenerateRandomLife.Location = new System.Drawing.Point(634, 392);
+            this.GenerateRandomLife.Name = "GenerateRandomLife";
+            this.GenerateRandomLife.Size = new System.Drawing.Size(142, 23);
+            this.GenerateRandomLife.TabIndex = 1;
+            this.GenerateRandomLife.Text = "GenerateRandomLife";
+            this.GenerateRandomLife.UseVisualStyleBackColor = true;
+            this.GenerateRandomLife.Click += new System.EventHandler(this.GenerateRandomLife_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(893, 392);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 2;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // nextgeneration
+            // 
+            this.nextgeneration.Location = new System.Drawing.Point(323, 392);
+            this.nextgeneration.Name = "nextgeneration";
+            this.nextgeneration.Size = new System.Drawing.Size(119, 23);
+            this.nextgeneration.TabIndex = 3;
+            this.nextgeneration.Text = "Next Generation";
+            this.nextgeneration.UseVisualStyleBackColor = true;
+            this.nextgeneration.Click += new System.EventHandler(this.nextgeneration_Click);
+            // 
+            // kill
+            // 
+            this.kill.Location = new System.Drawing.Point(792, 392);
+            this.kill.Name = "kill";
+            this.kill.Size = new System.Drawing.Size(75, 23);
+            this.kill.TabIndex = 4;
+            this.kill.Text = "Kill";
+            this.kill.UseVisualStyleBackColor = true;
+            this.kill.Click += new System.EventHandler(this.kill_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(45, 392);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 5;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pauseTime
+            // 
+            this.pauseTime.Location = new System.Drawing.Point(179, 392);
+            this.pauseTime.Name = "pauseTime";
+            this.pauseTime.Size = new System.Drawing.Size(75, 23);
+            this.pauseTime.TabIndex = 6;
+            this.pauseTime.Text = "Stop";
+            this.pauseTime.UseVisualStyleBackColor = true;
+            this.pauseTime.Click += new System.EventHandler(this.pauseTime_Click);
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Location = new System.Drawing.Point(492, 396);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(31, 15);
+            this.counter.TabIndex = 7;
+            this.counter.Text = "Gen:";
+            // 
+            // counterNum
+            // 
+            this.counterNum.AutoSize = true;
+            this.counterNum.Location = new System.Drawing.Point(541, 396);
+            this.counterNum.Name = "counterNum";
+            this.counterNum.Size = new System.Drawing.Size(13, 15);
+            this.counterNum.TabIndex = 9;
+            this.counterNum.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 461);
+            this.Controls.Add(this.counterNum);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.pauseTime);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.kill);
+            this.Controls.Add(this.nextgeneration);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.GenerateRandomLife);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button GenerateRandomLife;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.Button nextgeneration;
+        private System.Windows.Forms.Button kill;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button pauseTime;
+        private System.Windows.Forms.Label counter;
+        private System.Windows.Forms.Label counterNum;
+    }
+}
+
---
+++ CellAuto/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CellAuto
+{
+    public partial class Form1 : Form
+    {
+
+        int gen = 0;
+        bool[,] currentStep = new bool[35, 98];
+        bool[,] nextStep = new bool[35, 98];
+        Random rnd = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.EndEdit();
+            dataGridView1.RowCount = 35;
+            dataGridView1.ColumnCount = 98;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Height = 10;
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Columns[j].Width = 10;
+                       currentStep[i, j] = false;
+                }
+            }
+        }
+
+        /*
+         *Creates a single universe with some chance to cells become alive
+         */
+        private void GenerateRandomLife_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+            CreateRandomUniverse();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (currentStep[i, j])
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Green;
+                    else
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+                }
+            }
+        }
+        /*
+         * Makes our universe alive and setup the rule of life
+         */
+        public void nextGen()
+        {
+            int left, right, up, down;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (i != 0)
+                        up = i - 1;
+                    else
+                        up = dataGridView1.RowCount - 1;                                                 
+                    if (i != dataGridView1.RowCount - 1)
+                        down = i + 1;
+                    else
+                        down = 0;
+                    if (j != 0)
+                        left = j - 1;
+                    else
+                        left = dataGridView1.ColumnCount - 1;
+                    if (j != dataGridView1.ColumnCount - 1)
+                        right = j + 1;
+                    else
+                        right = 0;
+                    int lifeAround = 0; //на текущем этапе, мы превратили нашу вселенную - двумерный массив в тор.
+                    if (currentStep[i, left])
+                        lifeAround++;
+                    if (currentStep[i, right])
+                        lifeAround++;
+                    if (currentStep[up, j])
+                        lifeAround++;
+                    if (currentStep[down, j])
+                        lifeAround++;
+                    if (currentStep[up, right])
+                        lifeAround++;
+                    if (currentStep[up, left])
+                        lifeAround++;
+                    if (currentStep[down, right])
+                        lifeAround++;
+                    if (currentStep[down, left])
+                        lifeAround++; //Провели подсчет живых клеток вокруг нашей.
+                    if (lifeAround == 3 && currentStep[i, j] == false)
+                        nextStep[i, j] = true;
+                    else if ((lifeAround < 2 || lifeAround > 3) && currentStep[i, j] == true)
+                        nextStep[i, j] = false;
+                    else
+                        nextStep[i, j] = currentStep[i, j]; //описали условия жизни, смерти или поддержания жизни.
+                }
+            }
+        }
+        
+        /*
+         * С помощью данной функции превращаем завтрашний день в текущий.
+         */
+        public void newDay()
+        {
+            nextGen();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    currentStep[i, j] = nextStep[i, j];
+                }
+            }
+        }
+
+        /*
+         * метод для отображения всего связанного с жизнью => ее отображения и изменения
+         */
+        public void specLife()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (currentStep[i, j])
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Green;
+                    else
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+                }
+            }
+            dataGridView1.Refresh();
+        }
+
+        /*
+         * Method which creates a new universe and makes previous dead
+         */
+        public void CreateRandomUniverse()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (rnd.Next(100) < 5)
+                        currentStep[i, j] = true;
+                    else
+                        currentStep[i, j] = false;
+                }
+            }
+        }
+
+        /*
+         * Make the single cell alive or kill her
+         */
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (currentStep[e.RowIndex, e.ColumnIndex])
+            {
+                dataGridView1.CurrentCell.Style.BackColor = Color.White;
+                currentStep[e.RowIndex, e.ColumnIndex] = false;
+            }
+            else
+            {
+                dataGridView1.CurrentCell.Style.BackColor = Color.Green;
+                currentStep[e.RowIndex, e.ColumnIndex] = true;
+            }
+            dataGridView1.ClearSelection();
+        }
+        
+        /*
+         *Quit the programm 
+         */
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        /*
+         * По клику переходим в нвой день жизни
+         */
+        private void nextgeneration_Click(object sender, EventArgs e)
+        {
+            counterNum.Text = (++gen).ToString();
+            newDay();
+            specLife();
+        }
+
+        /*
+         * Убиваем всю жизнь
+         */
+        private void kill_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    currentStep[i, j] = false;
+                    dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+                }
+            }
+            dataGridView1.Refresh();
+            timer1.Stop();
+            gen = 0;
+            counterNum.Text = gen.ToString();
+        }
+     
+        /*
+         * Запускаем жизнь с текущими живыми клетками.
+         */
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        /*
+         * Метод описания действий в тик времени(на самом деле, мы просто имитируем переход в нвой день, но по тик таймеру)
+         */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nextgeneration_Click(sender, e);
+        }
+        /*
+         * Приостанавливаем развитие жизни
+         */
+        private void pauseTime_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+    }
+}
